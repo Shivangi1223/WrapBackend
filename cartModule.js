e8d538efa542e4054bca432c0b67b945ce95f612
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-25 00:35:44 +0530

    new

---
+++ cartModule.js
@@ -0,0 +1,14 @@
+let addToCart=()=>{
+    return "Add To Cart"
+}
+
+let Setmail=()=>{
+   return "Set Mail"
+}
+
+// Default exports -->>
+// module.exports = addToCart; // export the function"addToCart" anywhere -->>  
+
+
+// Nmaed EXPORTS -->> using named exports we can export multiple functions :)
+module.exports = {addToCart, Setmail}
