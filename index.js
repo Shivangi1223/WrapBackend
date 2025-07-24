c686c314e20284b39813a7dd002343749f45ea72
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-25 00:34:37 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const {addToCart, Setmail} = require("./cartModule");
+
 console.log("Welcome to my Backend journey");
+console.log(10+20)
+let l=[10,20,30,40,50]
+l.forEach((value, index)=>{
+ console.log(value, index)
+})
+console.log(addToCart());
+console.log(Setmail());
+console.log("Welcome");
