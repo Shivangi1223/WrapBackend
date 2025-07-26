34e24ad390aa74047c5c15b77380df0b3af7253a
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-26 11:49:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const {addToCart, Setmail} = require("./cartModule");
+// const {addToCart, Setmail} = require("./cartModule");
 
-console.log("Welcome to my Backend journey");
-console.log(10+20)
-let l=[10,20,30,40,50]
-l.forEach((value, index)=>{
- console.log(value, index)
+// console.log("Welcome to my Backend journey");
+// console.log(10+20)
+// let l=[10,20,30,40,50]
+// l.forEach((value, index)=>{
+//  console.log(value, index)
+// })
+// console.log(addToCart());
+// console.log(Setmail());
+// console.log("Welcome");
+
+
+
+// SERVER CREATION --->>>>>
+let http=require("http")
+
+
+let server=http.createServer((req, res)=>{
+    res.end("Welcome to my web page!")
 })
-console.log(addToCart());
-console.log(Setmail());
-console.log("Welcome");
+
+server.listen("5000") //http://localhost:5000
