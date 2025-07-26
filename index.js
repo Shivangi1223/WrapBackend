6dde96a5aa39b5adb878ae636c13784343888c9e
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-26 12:11:23 +0530

    updated

--- index.js
+++ index.js
@@ -17,7 +17,33 @@ let http=require("http")
 
 
 let server=http.createServer((req, res)=>{
-    res.end("Welcome to my web page!")
+
+    if(req.url=="/news"){    //http://localhost:5000/news   -->> req.url is a key or ye check krta h ki port ke baad kya url aaya h :)
+       let obj={
+        status:1,
+        data:[
+            {
+                newsTitle:'sh',
+                newsDes:"Say Hello!"
+            },
+            {
+                newsTitle:"IIP",
+                newsDes:"IIP is a good place to learn"
+            }
+        ]
+       }
+       res.end(JSON.stringify(obj))
+    }
+     if(req.url=="/about"){   //http://localhost:5000/about
+        
+    }
+     if(req.url=="/course"){  //http://localhost:5000/course 
+        
+    }
+     if(req.url=="/"){
+      res.end("Welcome to my web page!")  
+    }
+    // res.end("Welcome to my web page!")
 })
 
 server.listen("5000") //http://localhost:5000
