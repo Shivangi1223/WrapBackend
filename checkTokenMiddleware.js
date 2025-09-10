c5ef624010d78282046d6022926fee837b002572
Shivangi1223 <singhshivangi21805@gmail.com>
2025-09-10 23:56:56 +0530

    Token

---
+++ checkTokenMiddleware.js
@@ -0,0 +1,24 @@
+// let myToken="12345"
+
+let checkToken=(req,res,next)=>{
+    console.log(req.query.token)
+    if(req.query.token==" " || req.query.token==undefined){
+        return res.send(
+        {
+        status:0,
+        msg:"Please fill the token"
+        }
+    )
+    }
+    if(req.query.token!=process.env.myToken){
+        return res.send(
+        {
+        status:2,
+        msg:"Please fill the correct token"
+        }
+    )
+    }
+    next();
+}
+
+module.exports={checkToken}
